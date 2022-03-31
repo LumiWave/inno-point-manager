07a8c9ccfc6ff31c9a2c5229e1df6df50fd7563f
byeongtaste <jwshin@onbuff.com>
2022-03-31 13:58:12 +0900

    :hammer: db connect edit

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/ONBUFF-IP-TOKEN/inno-point-manager
 go 1.17
 
 require (
-	github.com/ONBUFF-IP-TOKEN/baseapp v1.0.12
-	github.com/ONBUFF-IP-TOKEN/basedb v1.1.7
+	github.com/ONBUFF-IP-TOKEN/baseapp v1.0.13
+	github.com/ONBUFF-IP-TOKEN/basedb v1.1.8
 	github.com/ONBUFF-IP-TOKEN/baseutil v1.0.7
 	github.com/denisenkom/go-mssqldb v0.11.0
 	github.com/google/go-querystring v1.1.0
--- rest_server/model/db.go
+++ rest_server/model/db.go
@@ -144,30 +144,32 @@ func MakeDbError(resp *base.BaseResponse, errCode int, err error) {
 
 func (o *DB) ConnectDB(conf *baseconf.DBAuth) (*basedb.Mssql, error) {
 	port, _ := strconv.ParseInt(conf.Port, 10, 32)
-	mssqlDB, err := basedb.NewMssql(conf.Database, "", conf.ID, conf.Password, conf.Host, int(port))
+	mssqlDB, err := basedb.NewMssql(conf.Database, "", conf.ID, conf.Password, conf.Host, int(port),
+		conf.ApplicationIntent, conf.Timeout, conf.ConnectRetryCount, conf.ConnectRetryInterval)
 	if err != nil {
 		log.Errorf("err: %v, val: %v, %v, %v, %v, %v, %v",
 			err, conf.Host, conf.ID, conf.Password, conf.Database, conf.PoolSize, conf.IdleSize)
 		return nil, err
 	}
+	idleSize, _ := strconv.ParseInt(conf.IdleSize, 10, 32)
+	mssqlDB.GetDB().SetMaxIdleConns(int(idleSize))
 
 	return mssqlDB, nil
 }
 
 func (o *DB) ConnectDBOfPoint(conf *baseconf.DBAuth, pointDB *context.PointDB) (*basedb.Mssql, error) {
 	port, _ := strconv.ParseInt(conf.Port, 10, 32)
-	mssqlDB, err := basedb.NewMssql(pointDB.DatabaseName,
-		"pointDB",
-		conf.ID,
-		conf.Password,
-		pointDB.ServerName,
-		int(port))
+	mssqlDB, err := basedb.NewMssql(pointDB.DatabaseName, "pointDB", conf.ID, conf.Password, pointDB.ServerName, int(port),
+		conf.ApplicationIntent, conf.Timeout, conf.ConnectRetryCount, conf.ConnectRetryInterval)
 
 	if err != nil {
 		log.Errorf("err: %v, val: %v, %v, %v, %v",
 			err, pointDB.ServerName, conf.ID, conf.Password, pointDB.DatabaseName)
 		return nil, err
 	}
+
+	idleSize, _ := strconv.ParseInt(conf.IdleSize, 10, 32)
+	mssqlDB.GetDB().SetMaxIdleConns(int(idleSize))
 
 	return mssqlDB, nil
 }
