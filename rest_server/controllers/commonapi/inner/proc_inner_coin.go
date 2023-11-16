736a0ab38193a0059b7720d38ad0ad8c0248df87
hochul <lagneid@onbuff.com>
2023-11-16 16:51:31 +0900

    :zap:wallet check

--- rest_server/controllers/commonapi/inner/proc_inner_coin.go
+++ rest_server/controllers/commonapi/inner/proc_inner_coin.go
@@ -20,38 +20,40 @@ func GetBalanceAll(auid int64) *base.BaseResponse {
 		resBalanceAll := &context.ResBalanceAll{}
 		resBalanceAll.Balances = []*context.Balance{}
 		for _, wallet := range wallets {
-			for _, coin := range model.GetDB().Coins {
-				if wallet.BaseCoinID != coin.BaseCoinID || coin.CoinId >= 20000 { // nft는 제외
-					continue
-				}
-				req := &token_manager_server.ReqBalance{
-					BaseSymbol: model.GetDB().BaseCoinMapByCoinID[wallet.BaseCoinID].BaseCoinSymbol,
-					Contract: func() string {
-						// 코인 타입이면 contract 정보를 를 보내지 않는다.
-						if strings.EqualFold(model.GetDB().BaseCoinMapByCoinID[wallet.BaseCoinID].BaseCoinSymbol, model.GetDB().Coins[coin.CoinId].CoinSymbol) {
-							return ""
-						}
-						return model.GetDB().Coins[coin.CoinId].ContractAddress
-					}(),
-					Address: wallet.WalletAddress,
-				}
+			if wallet.ConnectionStatus == 1 {
+				for _, coin := range model.GetDB().Coins {
+					if wallet.BaseCoinID != coin.BaseCoinID || coin.CoinId >= 20000 { // nft는 제외
+						continue
+					}
+					req := &token_manager_server.ReqBalance{
+						BaseSymbol: model.GetDB().BaseCoinMapByCoinID[wallet.BaseCoinID].BaseCoinSymbol,
+						Contract: func() string {
+							// 코인 타입이면 contract 정보를 를 보내지 않는다.
+							if strings.EqualFold(model.GetDB().BaseCoinMapByCoinID[wallet.BaseCoinID].BaseCoinSymbol, model.GetDB().Coins[coin.CoinId].CoinSymbol) {
+								return ""
+							}
+							return model.GetDB().Coins[coin.CoinId].ContractAddress
+						}(),
+						Address: wallet.WalletAddress,
+					}
 
-				if res, err := token_manager_server.GetInstance().GetBalance(req); err != nil {
-					resp.SetReturn(resultcode.ResultInternalServerError)
-					return resp
-				} else {
-					if res.Return != 0 { // token manager 전송 에러
-						log.Errorf("GetBalance return : %v, msg:%v", res.Return, res.Message)
+					if res, err := token_manager_server.GetInstance().GetBalance(req); err != nil {
+						resp.SetReturn(resultcode.ResultInternalServerError)
+						return resp
 					} else {
-						log.Debugf("Balance symbol:%v, bal:%v, decimal:%v", coin.CoinSymbol, res.Balance, res.Decimal)
-						resBalanceAll.Balances = append(resBalanceAll.Balances, &context.Balance{
-							CoinID:     coin.CoinId,
-							BaseCoinID: coin.BaseCoinID,
-							Symbol:     coin.CoinSymbol,
-							Balance:    res.Balance,
-							Address:    wallet.WalletAddress,
-							Decimal:    res.Decimal,
-						})
+						if res.Return != 0 { // token manager 전송 에러
+							log.Errorf("GetBalance return : %v, msg:%v", res.Return, res.Message)
+						} else {
+							log.Debugf("Balance symbol:%v, bal:%v, decimal:%v", coin.CoinSymbol, res.Balance, res.Decimal)
+							resBalanceAll.Balances = append(resBalanceAll.Balances, &context.Balance{
+								CoinID:     coin.CoinId,
+								BaseCoinID: coin.BaseCoinID,
+								Symbol:     coin.CoinSymbol,
+								Balance:    res.Balance,
+								Address:    wallet.WalletAddress,
+								Decimal:    res.Decimal,
+							})
+						}
 					}
 				}
 			}
