37b86cc24cd1244e570baf2338297c9b2ae15aad
hochul <lagneid@onbuff.com>
2024-05-03 17:06:10 +0900

    :zap:certi chg

--- prebuild.sh
+++ prebuild.sh
@@ -5,8 +5,8 @@ mkdir -p bin
 cp ./etc/conf/config.$1.yml ./bin/config.yml
 cp ./etc/conf/external_api.yml ./bin
 cp ./etc/conf/internal_api.yml ./bin
-cp ./etc/onbuffcerti.crt ./bin
-cp ./etc/onbuffcerti.key ./bin
+cp ./etc/lumiwavecerti.crt ./bin
+cp ./etc/lumiwavecerti.key ./bin
 
 mkdir -p bin/docs/ext
 
